ee6dd22942c7eca04a95f411a30203cbde0340d3
Eivind Szalay <eivind.szalay@gmail.com>
2024-12-02 21:08:02 +0100

    solve dec 2

---
+++ src/dec02.rs
@@ -0,0 +1,75 @@
+use std::fs::read_to_string;
+
+pub fn dec02a() {
+
+    let mut sum = 0;
+
+    for line in read_to_string("src/dec02a.in").unwrap().lines() {
+        let levels: Vec<i32> = line.split(" ").map(|level| level.parse().unwrap()).collect();
+
+        let mut safe = true;
+        let mut i = 0;
+        while i < levels.len() - 1 {
+            let diff = levels[i+1] - levels[i];
+            if levels[0] < levels[1] {
+                safe = safe && 1 <= diff && diff <=3;
+            } else {
+                safe = safe && -3 <= diff && diff <=-1;
+            }
+
+            i += 1;
+        }
+
+        if safe {
+            sum += 1;
+        }
+
+    }
+
+    println!("{}", sum);
+}
+
+pub fn dec02b() {
+
+    let mut sum = 0;
+
+    for line in read_to_string("src/dec02a.in").unwrap().lines() {
+        let mut levels: Vec<i32> = line.split(" ").map(|level| level.parse().unwrap()).collect();
+
+        if safe(&levels) {
+            sum += 1;
+        } else {
+            let mut i = 0;
+            let length = levels.len();
+            while i < length {
+                let removed = levels.remove(i);
+                if safe(&levels) {
+                    sum += 1;
+                    i = length;
+                }
+                else {
+                    levels.insert(i, removed);
+                    i += 1;
+                }
+            }
+        }
+    }
+
+    println!("{}", sum);
+}
+
+fn safe(levels: &Vec<i32>) -> bool {
+    let mut safe = true;
+    let mut i = 0;
+    while i < levels.len() - 1 {
+        let diff = levels[i+1] - levels[i];
+        if levels[0] < levels[1] {
+            safe = safe && 1 <= diff && diff <=3;
+        } else {
+            safe = safe && -3 <= diff && diff <=-1;
+        }
+
+        i += 1;
+    }
+    return safe;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+#[allow(dead_code)]
 mod dec01;
+mod dec02;
 
 fn main() {
-    dec01::dec01a();
-    dec01::dec01b();
+    // dec01::dec01a();
+    // dec01::dec01b();
+    dec02::dec02a();
+    dec02::dec02b();
 }
