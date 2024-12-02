c7fdcd7002b64bff5eb38502f86a7d38db1a02e0
Eivind Szalay <eivind.szalay@gmail.com>
2024-12-02 20:19:29 +0100

    solution to dec 1 b

--- src/dec01.rs
+++ src/dec01.rs
@@ -1,10 +1,12 @@
 use std::fs::read_to_string;
+use std::collections::HashMap;
+
 
 
 pub fn dec01a() {
     let mut left_ids: Vec<u32> = Vec::new(); 
     let mut right_ids: Vec<u32> = Vec::new(); 
-
+    
     for line in read_to_string("src/dec01a.in").unwrap().lines() {
         let ids: Vec<&str> = line.split_whitespace().collect();
         left_ids.push(ids[0].parse::<u32>().unwrap());
@@ -23,4 +25,40 @@ pub fn dec01a() {
     }
 
     println!("{}", sum);
+}
+
+
+pub fn dec01b() {
+    let mut left_ids: Vec<u32> = Vec::new(); 
+    let mut right_ids: Vec<u32> = Vec::new(); 
+    
+    for line in read_to_string("src/dec01a.in").unwrap().lines() {
+        let ids: Vec<&str> = line.split_whitespace().collect();
+        left_ids.push(ids[0].parse::<u32>().unwrap());
+        right_ids.push(ids[1].parse::<u32>().unwrap());
+    }
+    
+    let mut occurences: HashMap<u32, u32> = HashMap::new();
+
+    for id in right_ids.iter() {
+        let value = occurences.get(id);
+
+        if let Some(v) = value {
+            occurences.insert(*id, v + 1);
+        } else {
+            occurences.insert(*id, 1);
+        }
+    }
+
+    let mut sum = 0;
+
+    for id in left_ids.iter() {
+        let value = occurences.get(id);
+        if let Some(v) = value {
+            sum += id*v;
+        }
+    }
+
+    println!("{}", sum);
+    
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod dec01;
 
 fn main() {
-    dec01::dec01a();
+    // dec01::dec01a();
+    dec01::dec01b();
 }
