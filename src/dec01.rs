5a352eb13d09310de5cac826b55cc77c46324e4c
Eivind Szalay <eivind.szalay@gmail.com>
2024-12-02 20:03:09 +0100

    solution to dec 1 a

--- src/dec01.rs
+++ src/dec01.rs
@@ -1,10 +1,26 @@
-use std::fs;
+use std::fs::read_to_string;
 
 
 pub fn dec01a() {
-    println!("Solving the first problem from December 1:");
-    let data = fs::read_to_string("src/dec01a.in").expect("Unable to read file");
-    println!("{}", data);   
-    
-}
+    let mut left_ids: Vec<u32> = Vec::new(); 
+    let mut right_ids: Vec<u32> = Vec::new(); 
 
+    for line in read_to_string("src/dec01a.in").unwrap().lines() {
+        let ids: Vec<&str> = line.split_whitespace().collect();
+        left_ids.push(ids[0].parse::<u32>().unwrap());
+        right_ids.push(ids[1].parse::<u32>().unwrap());
+    }
+
+    left_ids.sort();
+    right_ids.sort();
+    println!("{}", left_ids.first().unwrap());
+    println!("{}", left_ids.last().unwrap());
+
+    let mut sum = 0;
+
+    for (index, id) in left_ids.iter().enumerate() {
+        sum += id.abs_diff(right_ids[index]);
+    }
+
+    println!("{}", sum);
+}
