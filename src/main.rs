03b3891403fb9a23962423826961ebca28610732
Eivind Szalay <eivind.szalay@gmail.com>
2024-12-03 19:05:59 +0100

    add timing and formatting

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,29 @@
 #[allow(dead_code)]
+use std::time::Instant;
 mod dec01;
 mod dec02;
 mod dec03;
 
 fn main() {
-    dec01::dec01a();
-    dec01::dec01b();
-    dec02::dec02a();
-    dec02::dec02b();
-    dec03::dec03a();
-    dec03::dec03b();
+    
+    let functions = [
+        dec01::dec01a,
+        dec01::dec01b, 
+        dec02::dec02a,
+        dec02::dec02b, 
+        dec03::dec03a,
+        dec03::dec03b
+    ].to_vec();
+    
+    execute_functions(functions);
+}
+
+fn execute_functions(fns: Vec<fn()>) {
+    let mut now = Instant::now();
+    for (index, function) in fns.iter().enumerate() {
+        println!("december {}, part {}", index/2+1, index%2 + 1);
+        function();
+        println!("executed in {:?}\n", now.elapsed());
+        now = Instant::now();
+    }
 }
