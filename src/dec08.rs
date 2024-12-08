d1ee07147fad73b61fce0ecd42338216b413ab33
Eivind Szalay <eivind.szalay@gmail.com>
2024-12-08 11:38:46 +0100

    solve dec 8, part 2

--- src/dec08.rs
+++ src/dec08.rs
@@ -35,9 +35,9 @@ pub fn part1() -> usize {
                 // point 1
                 {
                     if x1 == greater_x {
-                        let new_x = add(*x1, x_diff, length);
+                        let new_x = add(*x1, x_diff, length, 1);
                         if y1 == greater_y {
-                            let new_y = add(*y1, y_diff, height);
+                            let new_y = add(*y1, y_diff, height, 1);
                             if let Some(x) = new_x {
                                 if let Some(y) = new_y {
                                     antinodes.insert((x, y));
@@ -54,7 +54,7 @@ pub fn part1() -> usize {
                     } else {
                         let new_x = x1.checked_sub(x_diff);
                         if y1 == greater_y {
-                            let new_y = add(*y1, y_diff, height);
+                            let new_y = add(*y1, y_diff, height, 1);
                             if let Some(x) = new_x {
                                 if let Some(y) = new_y {
                                     antinodes.insert((x, y));
@@ -73,9 +73,9 @@ pub fn part1() -> usize {
                 // point 2
                 {
                     if x2 == greater_x {
-                        let new_x = add(*x2, x_diff, length);
+                        let new_x = add(*x2, x_diff, length, 1);
                         if y2 == greater_y {
-                            let new_y = add(*y2, y_diff, height);
+                            let new_y = add(*y2, y_diff, height, 1);
                             if let Some(x) = new_x {
                                 if let Some(y) = new_y {
                                     antinodes.insert((x, y));
@@ -92,7 +92,7 @@ pub fn part1() -> usize {
                     } else {
                         let new_x = x2.checked_sub(x_diff);
                         if y2 == greater_y {
-                            let new_y = add(*y2, y_diff, height);
+                            let new_y = add(*y2, y_diff, height, 1);
                             if let Some(x) = new_x {
                                 if let Some(y) = new_y {
                                     antinodes.insert((x, y));
@@ -119,12 +119,179 @@ pub fn part1() -> usize {
     antinodes.len()
 }
 
-fn add(c1: usize, c2: usize, max: usize) -> Option<usize> {
-    let sum = c1+c2;
+fn add(c1: usize, c2: usize, max: usize, n: usize) -> Option<usize> {
+    let sum = c1+c2*n;
     if sum >= max {return None;}
     return Some(sum);
 }
 
+/// correct answer is 1017
 pub fn part2() -> usize {
-    0
+    
+    let input = read_to_string("src/in/dec08.in").unwrap();
+    let map: Vec<&str>  = input.lines().collect();
+    let height = map.len();
+    let length = map[0].len();
+    let mut antennas: HashMap<char, Vec<(usize, usize)>> = HashMap::new();
+    for y in 0..height {
+        for x in 0..length {
+            let c: char = map[y].chars().nth(x).unwrap();
+            if c != '.' {
+                let seen = antennas.get_mut(&c);
+                if let Some(s) = seen {
+                    s.push((x, y));
+                } else {
+                    antennas.insert(c, vec!( (x, y)));
+                }
+            }
+        }
+    }
+    let mut antinodes: HashSet<(usize, usize)> = HashSet::new();
+    for (symbol, points) in antennas {
+        println!("{:?}", (symbol, &points));
+        for (i, (x1, y1)) in points.iter().enumerate() {
+            for (x2, y2) in points[i+1..].iter() {
+                antinodes.insert((*x1, *y1));
+                antinodes.insert((*x2, *y2));
+                let greater_x = cmp::max(x1, x2);
+                let smaller_x = cmp::min(x1, x2);
+                let x_diff = greater_x - smaller_x;
+                let greater_y = cmp::max(y1, y2);
+                let smaller_y = cmp::min(y1, y2);
+                let y_diff = greater_y - smaller_y;
+
+                // point 1
+                {
+                    if x1 == greater_x {
+                        let mut inside_map = true;
+                        let mut m = 1;
+                        while inside_map {
+                            let new_x = add(*x1, x_diff, length, m);
+                            if let Some(x) = new_x {
+                                if y1 == greater_y {
+                                    let new_y = add(*y1, y_diff, height, m);
+                                        if let Some(y) = new_y {
+                                            antinodes.insert((x, y));
+                                        } else {
+                                            inside_map = false;
+                                        }
+                                } else {
+                                    let new_y = y1.checked_sub(y_diff*m);
+                                    if let Some(y) = new_y {
+                                            antinodes.insert((x, y));
+                                        } else {
+                                            inside_map = false;
+                                        }
+                                    }
+                            } else {
+                                inside_map=false;
+                            }
+                            m+=1;
+                        }
+                    } else {
+                        let mut inside_map = true;
+                        let mut m = 1;
+                        while inside_map {
+                            let new_x = x1.checked_sub(x_diff*m);
+                            if let Some(x) = new_x {
+                                if y1 == greater_y {
+                                    let new_y = add(*y1, y_diff, height, m);
+                                        if let Some(y) = new_y {
+                                            antinodes.insert((x, y));
+                                        } else {
+                                            inside_map = false;
+                                        }
+                                } else {
+                                    let new_y = y1.checked_sub(y_diff*m);
+                                        if let Some(y) = new_y {
+                                            antinodes.insert((x, y));
+                                        } else {
+                                            inside_map = false;
+                                        }
+                                }
+                            } else {
+                                inside_map = false;
+                            }
+                            m+=1;
+                        }
+                    }
+                }
+                // point 2
+                {
+                    if x2 == greater_x {
+                        let mut inside_map = true;
+                        let mut m = 1;
+                        while inside_map {
+                            let new_x = add(*x2, x_diff, length, m);
+                            if let Some(x) = new_x {
+                                if y2 == greater_y {
+                                    let new_y = add(*y2, y_diff, height, m);
+                                        if let Some(y) = new_y {
+                                            antinodes.insert((x, y));
+                                        } else {
+                                            inside_map = false;
+                                        }
+                                } else {
+                                    let new_y = y2.checked_sub(y_diff*m);
+                                        if let Some(y) = new_y {
+                                            antinodes.insert((x, y));
+                                        } else {
+                                            inside_map = false;
+                                        }
+                                }
+                            } else {
+                                inside_map = false;
+                            }
+                            m+=1;
+                        }
+                    } else {
+                        let mut inside_map = true;
+                        let mut m = 1;
+                        while inside_map {
+                            let new_x = x2.checked_sub(x_diff*m);
+                            if let Some(x) = new_x {
+                                if y2 == greater_y {
+                                    let new_y = add(*y2, y_diff, height, m);
+                                        if let Some(y) = new_y {
+                                            antinodes.insert((x, y));
+                                        } else {
+                                            inside_map = false;
+                                        }
+                                } else {
+                                    let new_y = y2.checked_sub(y_diff*m);
+                                        if let Some(y) = new_y {
+                                            antinodes.insert((x, y));
+                                        } else {
+                                            inside_map = false;
+                                        }
+                                }
+                            } else {
+                                inside_map = false;
+                            }
+                            m+=1;
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+
+
+
+    for node in &antinodes {
+        println!("{:?}", node);
+    }
+    for i in 0..height {
+        for j in 0..length {
+            if let Some(v) = antinodes.get(&(j, i)) {
+                print!("T");
+            } else {
+                print!(".")
+            }
+        }
+        print!("\n");
+    }
+    antinodes.len()
+
 }
