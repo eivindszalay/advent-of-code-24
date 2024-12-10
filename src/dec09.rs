14ea6d60547dc6a77eb17ef6416c550efa328e22
Eivind Szalay <eivind.szalay@gmail.com>
2024-12-10 23:46:22 +0100

    performance improvements to dec 9

--- src/dec09.rs
+++ src/dec09.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, fs::read_to_string};
+use std::fs::read_to_string;
 
 /// correct answer is 6288599492129
 pub fn part1() -> usize {
@@ -52,86 +52,51 @@ pub fn part1() -> usize {
 }
 // correct answer is 6321896265143
 pub fn part2() -> usize {
-    let disk_map = read_to_string("src/in/dec09.in").unwrap();
-    let mut expanded: Vec<String> = vec!();
-    for (i, char) in disk_map.chars().enumerate() {
+    let disk_map: Vec<usize> = read_to_string("src/in/dec09.in")
+        .unwrap()
+        .chars()
+        .filter_map(|c| c.to_digit(10).map(|d| d as usize))
+        .collect();
+    let mut files: Vec<(usize, usize, usize)> = vec!();
+    let mut free: Vec<(usize, usize)> = vec!();
+    let mut curr_position = 0;
+    for (i, size) in disk_map.iter().enumerate() {
         if i%2==0 {
-            let mut file: Vec<String> = vec!();
-            for _j in 0..char.to_digit(10).unwrap() {
-                let id = i/2;
-                file.push(id.to_string());
-            }
-            expanded.extend(file);
+            files.push(((i/2),curr_position, *size));
         } else {
-            let foo = char.to_digit(10);
-            if let Some(e) = foo {
-                for _j in 0..e {
-                    expanded.push(".".to_string());
-                }
-            }
+            free.push(( curr_position,  *size));
         }
-    }
-
-    let mut rearranged_map: HashMap<String, (usize, usize)> = HashMap::new();
-    let mut occupied_map: HashMap<usize, usize> = HashMap::new();
-    let mut j = expanded.len()-1;
-    while 0<j {
-        let id = expanded.get(j).unwrap();
-        if id!="." {
-            let mut file_length = 0;
-            while expanded.get(j).unwrap()==id {
-                let opt = j.checked_sub(1);
-                if let Some(new_j) = opt {
-                    file_length +=1;
-                    j = new_j;
-                } else {
-                    file_length += 1;
-                    rearranged_map.insert(id.to_string(), (j, file_length));
-                    break;
-                }
-            }
-            let mut k = 0;
-            let mut free_length = 0;
-            let mut found_room = false;
-            while k<j+1 {
-                let is_occ_opt = occupied_map.get(&k);
-                if let Some(len) = is_occ_opt {
-                    k += len;
-                    free_length = 0;
-                } else {
-                    if expanded.get(k).unwrap()=="." || *expanded.get(k).unwrap()==id.to_string() {
-                        free_length += 1;
-                    } else {
-                        free_length = 0;
-                    }
-                    k += 1;
-                } 
-                if free_length==file_length {
-                    rearranged_map.insert(id.to_string(), (k-file_length, file_length));
-                    occupied_map.insert(k-file_length, file_length);
-                    found_room = true;
-                    break;
-                }
-            }   
-            if !found_room {
-                rearranged_map.insert(id.to_string(), (j+1, file_length));
-            }
-        } else {
-            j -= 1;
-            if j==0 {
-                let id = expanded.get(j).unwrap();
-                if id != "." {
-                    rearranged_map.insert(id.to_string(), (0, 1));
-                }
-            }
-        }
+        curr_position += size;
     }
     let mut sum = 0;
-    for (id, (index, length)) in rearranged_map {
-        for i in 0..length {
-            sum += id.parse::<usize>().unwrap()*(index+i);
-        }
-    }
+    loop {
+        if let Some((file_id, file_starting_pos, file_size)) = files.pop() {
+            let mut free_space = (false, 0, 0, 0);
+            
+            for (i, (pos, size)) in free.iter().enumerate() {
+                if *pos>=file_starting_pos {
+                    break;
+                }
+                if *size>=file_size {
+                    free_space = (true, i, *pos, *size);
+                    break;
+                }
+            }
 
-    sum
+            if free_space.0 {
+                let space_left = free_space.3-file_size;
+                free[free_space.1] = (free_space.2+file_size, space_left);
+                for i in 0..file_size {
+                    sum += file_id*(free_space.2+i);
+                }
+            } else {
+                for i in 0..file_size {
+                    sum += file_id*(file_starting_pos+i);
+                }
+            }
+        } else {
+            return sum;
+        }
+
+    }
 }
